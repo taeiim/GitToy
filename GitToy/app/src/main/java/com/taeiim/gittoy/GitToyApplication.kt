e55e9badad79262a8a3e5195a4e54ac95ee0eb5f
taeiim <taeiim.dev@gmail.com>
2020-06-21 19:55:30 +0900

    [Hilt] Application 변경

--- GitToy/app/src/main/java/com/taeiim/gittoy/GitToyApplication.kt
+++ GitToy/app/src/main/java/com/taeiim/gittoy/GitToyApplication.kt
@@ -1,19 +1,15 @@
 package com.taeiim.gittoy
 
+import android.app.Application
 import com.facebook.stetho.Stetho
-import com.taeiim.gittoy.di.DaggerAppComponent
-import dagger.android.DaggerApplication
-import dagger.android.HasAndroidInjector
+import dagger.hilt.android.HiltAndroidApp
 
-class GitToyApplication : DaggerApplication(), HasAndroidInjector {
-
-    private val appComponent = DaggerAppComponent.factory().create(this)
+@HiltAndroidApp
+class GitToyApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
         Stetho.initializeWithDefaults(this)
     }
-
-    override fun applicationInjector() = appComponent
 
 }
