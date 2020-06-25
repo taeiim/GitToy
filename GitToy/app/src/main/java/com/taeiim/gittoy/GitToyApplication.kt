eca447d70dbdabc39e3546f426e7deb624625cdc
taeiim <taeiim.dev@gmail.com>
2020-06-25 20:28:18 +0900

    [Hilt] Application 변경

--- GitToy/app/src/main/java/com/taeiim/gittoy/GitToyApplication.kt
+++ GitToy/app/src/main/java/com/taeiim/gittoy/GitToyApplication.kt
@@ -2,16 +2,13 @@ package com.taeiim.gittoy
 
 import android.app.Application
 import com.facebook.stetho.Stetho
-import com.taeiim.gittoy.di.dataSourceModule
-import com.taeiim.gittoy.di.networkModule
-import com.taeiim.gittoy.di.viewModelModule
-import com.taeiim.gittoy.ext.setupKoin
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class GitToyApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        setupKoin(this, viewModelModule, dataSourceModule, networkModule)
         Stetho.initializeWithDefaults(this)
     }
 
