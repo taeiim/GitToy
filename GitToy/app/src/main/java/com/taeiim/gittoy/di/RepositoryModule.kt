3b6892678a1a4798c164883257151b961c0dffbd
taeiim <taeiim.god@gmail.com>
2020-07-01 23:15:47 +0900

    fix abstract @Provides -> @Binds

--- GitToy/app/src/main/java/com/taeiim/gittoy/di/RepositoryModule.kt
+++ GitToy/app/src/main/java/com/taeiim/gittoy/di/RepositoryModule.kt
@@ -2,18 +2,16 @@ package com.taeiim.gittoy.di
 
 import com.taeiim.gittoy.data.GithubRepository
 import com.taeiim.gittoy.data.GithubRepositoryImpl
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
-import javax.inject.Singleton
 
 @Module
 @InstallIn(ApplicationComponent::class)
 abstract class RepositoryModule {
 
-    @Singleton
-    @Provides
+    @Binds
     abstract fun provideDataRepository(dataRepository: GithubRepositoryImpl): GithubRepository
 
 }
